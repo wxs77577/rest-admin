cd5f5c4f3788fa74ced23c5b9c805a548f99bc80
yizhi_iMac <wu-xuesong@qq.com>
2018-08-23 16:08:15 +0800

    fix test api

--- api/index.js
+++ api/index.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 router.get('/site', (req, res) => res.send({
   name: 'REST-ADMIN', //site name
   locale: 'en-US',
+  logo: 'http://rest-admin.genyii.com/logo.png',
   locale_switcher: true,
   url: 'https://github.com/wxs77577/rest-admin',
   footer: `

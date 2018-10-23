02dcb0cae19ce453b995363d6405635992ca8ce0
wxs77577 <wu-xuesong@qq.com>
2018-10-23 11:40:02 +0800

    disable loading

--- src/http.js
+++ src/http.js
@@ -6,13 +6,15 @@ import _ from 'lodash'
 const API_URI = process.env.VUE_APP_API_URL || '/admin/api/'
 global.API_URI = API_URI
 axios.defaults.baseURL = API_URI
+global.LOADING_ENABLED = true
 axios.interceptors.request.use(config => {
-  store.commit(types.START_LOADING)
+  global.LOADING_ENABLED && store.commit(types.START_LOADING)
   config.headers.Authorization = 'Bearer ' + store.state.auth.token
   return config
 })
 axios.interceptors.response.use(response => {
   store.commit(types.STOP_LOADING)
+  global.LOADING_ENABLED = true
   const pageHeader = _.get(response, 'data._meta.page_header')
   if (pageHeader || pageHeader === false) {
     store.commit(types.SET_PAGE_HEADER, pageHeader)

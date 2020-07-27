2b6627ed926039e87c7c47b672f30be41ae3dffb
Johnny <wu-xuesong@qq.com>
2020-07-27 14:32:23 +0800

    avue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
